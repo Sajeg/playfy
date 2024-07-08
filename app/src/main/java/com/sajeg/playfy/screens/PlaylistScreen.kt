05fd87674919fc59a98e12b85aedb32d805e0cdc
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-07-08 02:27:14 +0200

    Improved Style

--- app/src/main/java/com/sajeg/playfy/screens/PlaylistScreen.kt
+++ app/src/main/java/com/sajeg/playfy/screens/PlaylistScreen.kt
@@ -1,18 +1,23 @@
 package com.sajeg.playfy.screens
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
 import com.sajeg.playfy.SpotifyApi
 import com.sajeg.playfy.SpotifySong
-import com.sajeg.playfy.spotifyApi
+import com.sajeg.playfy.paddingModifier
 
 @Composable
 fun PlayListView(id: String) {
@@ -23,13 +28,26 @@ fun PlayListView(id: String) {
             tracks = it
         })
     }
-    Column {
-        Text(text = SpotifyApi.userName!!)
-    }
-    LazyColumn {
+    LazyColumn(
+        modifier = paddingModifier.padding(horizontal = 5.dp)
+    ) {
         if (tracks != null) {
             items(tracks!!) { track ->
-                Text(text = "${track.title} from ${track.artist}")
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp)
+                ) {
+                    Text(
+                        text = track.title,
+                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 1.dp)
+                    )
+                    Text(
+                        text = "from ${track.artist}",
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier.padding(horizontal = 5.dp, vertical = 1.dp)
+                    )
+                }
             }
         }
     }
