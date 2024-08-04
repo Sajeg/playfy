38ed1875e95c30d1c569cfbff352becebd1cfcbe
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-08-04 14:49:34 +0200

    Added floating playlist create button

--- app/src/main/java/com/sajeg/playfy/GeminiApi.kt
+++ app/src/main/java/com/sajeg/playfy/GeminiApi.kt
@@ -20,9 +20,31 @@ object GeminiApi {
         systemInstruction = Content(
             role = "user", parts = listOf(
                 TextPart(
-                    "You'll receive a list of songs. You task is then to add 10 songs with " +
+                    "You'll receive a list of songs. Your task is then to add 10 songs with " +
                             "their title and artist that are similar to the songs, that are already in the playlist with a good mix between popular and unknown songs. " +
                             "Do not return the Songs, that you received, but also the genre of the Playlist. Not for each song individually."
+                )
+            )
+        ),
+        safetySettings = listOf(
+            SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.NONE),
+            SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.NONE),
+            SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.NONE)
+        ),
+        generationConfig = generationConfig {
+            responseMimeType = "application/json"
+        }
+    )
+
+    private val modelPlaylistCreator = GenerativeModel(
+        modelName = "gemini-1.5-flash",
+        apiKey = BuildConfig.geminiApiKey,
+        systemInstruction = Content(
+            role = "user", parts = listOf(
+                TextPart(
+                    "You'll receive a set of criteria and your task is then to give back 25 songs with " +
+                            "their title and artist that meet the criteria. Make a good mix between popular and unknown songs. " +
+                            "Also return the a name for the Playlist."
                 )
             )
         ),
@@ -46,6 +68,19 @@ object GeminiApi {
             return songs
         } else {
             Log.d("Gemini", "Not successfully converted!")
+            return null
+        }
+    }
+
+    suspend fun newPlaylist(prompt: String): List<Songs>? {
+        val response = modelPlaylistCreator.generateContent(prompt)
+        val text = response.candidates[0].content.parts[0].asTextOrNull()
+        Log.d("Response", text!!)
+        if (text != null) {
+            val output = JSONObject(text)
+            val songs = Json.decodeFromString<List<Songs>>(output.getJSONArray("songs").toString())
+            return songs
+        } else {
             return null
         }
     }
--- app/src/main/java/com/sajeg/playfy/screens/Homescreen.kt
+++ app/src/main/java/com/sajeg/playfy/screens/Homescreen.kt
@@ -1,16 +1,24 @@
 package com.sajeg.playfy.screens
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,15 +26,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.sajeg.playfy.GeminiApi
 import com.sajeg.playfy.Playlist
 import com.sajeg.playfy.PlaylistScreen
+import com.sajeg.playfy.R
 import com.sajeg.playfy.SpotifyApi
 import com.sajeg.playfy.paddingModifier
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 
 
@@ -36,6 +50,7 @@ fun HomeScreen(navController: NavController) {
     var name by remember { mutableStateOf("") }
     var playlists by remember { mutableStateOf<List<Playlist>?>(null) }
     var sentRequest by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
 
     if (SpotifyApi.token != "" && name == "") {
         SpotifyApi.getUsername { username ->
@@ -62,42 +77,76 @@ fun HomeScreen(navController: NavController) {
             }
         }
     }
-
-    Column(
-        modifier = paddingModifier.padding(horizontal = 20.dp)
-    ) {
-        Text(
-            text = "Hi, $name",
-            fontSize = 32.sp
-        )
-        Text(text = "Select a Playlist, that you want to modify")
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(horizontal = 5.dp, vertical = 20.dp),
-        ) {
-            playlists?.let { list ->
-                items(list) { playlist ->
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                showDialog = true;
+            }) {
+                Icon(painter = painterResource(id = R.drawable.add), contentDescription = "")
+            }
+        }
+    ) { padding ->
+        AnimatedVisibility(showDialog) {
+            var prompt by remember { mutableStateOf("") }
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                confirmButton = {
+                    TextButton(onClick = {
+                        showDialog = false
+                        CoroutineScope(Dispatchers.IO).launch {
+                            GeminiApi.newPlaylist(prompt)
+                        }
+                    }
                     ) {
-                        GlideImage(
-                            model = playlist.imgUrl,
-                            contentDescription = playlist.name,
-                            modifier = Modifier
-                                .size(150.dp)
-                                .aspectRatio(1f)
-                                .clickable {
-                                    navController.navigate(
-                                        PlaylistScreen(
-                                            playlist.id,
-                                            playlist.name,
-                                            playlist.imgUrl
-                                        )
-                                    )
-                                }
+                        Text(text = "Confirm")
+                    }
+
+                },
+                title = { Text(text = "New Playlist") },
+                text = {
+                    TextField(value = prompt, onValueChange = { prompt = it }, modifier = Modifier.fillMaxWidth(), placeholder = {
+                        Text(
+                            text = "Specify a genre, topic, artist..."
                         )
-                        Text(text = playlist.name)
+                    })
+                })
+        }
+        Column(
+            modifier = paddingModifier.padding(horizontal = 20.dp)
+        ) {
+            Text(
+                text = "Hi, $name",
+                fontSize = 32.sp
+            )
+            Text(text = "Select a Playlist, that you want to modify")
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(horizontal = 5.dp, vertical = 20.dp),
+            ) {
+                playlists?.let { list ->
+                    items(list) { playlist ->
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            GlideImage(
+                                model = playlist.imgUrl,
+                                contentDescription = playlist.name,
+                                modifier = Modifier
+                                    .size(150.dp)
+                                    .aspectRatio(1f)
+                                    .clickable {
+                                        navController.navigate(
+                                            PlaylistScreen(
+                                                playlist.id,
+                                                playlist.name,
+                                                playlist.imgUrl
+                                            )
+                                        )
+                                    }
+                            )
+                            Text(text = playlist.name)
+                        }
                     }
                 }
             }
